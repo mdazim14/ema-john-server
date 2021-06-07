e51e5261461fef87809b33a9db44f7965e22ae98
Md Azim <mdazim.cse@gmail.com>
2021-06-08 02:22:53 +0600

    rote api

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const app = express()
 app.use(bodyParser.json());
 app.use(cors());
+
 const port = 5000
 
+app.get('/', (req, res) => {
+    res.send("hello from db it's working")
+})
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
